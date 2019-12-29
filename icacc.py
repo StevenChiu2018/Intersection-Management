05a0069a65d4dda2d0fd23bf654bf662c163e86c
StevenChiu2018 <35871453+StevenChiu2018@users.noreply.github.com>
2019-12-30 00:45:52 +0800

    All done

--- icacc.py
+++ icacc.py
@@ -1,26 +1,71 @@
-#!/usr/bin/env python
-# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
-# Copyright (C) 2009-2019 German Aerospace Center (DLR) and others.
-# This program and the accompanying materials
-# are made available under the terms of the Eclipse Public License v2.0
-# which accompanies this distribution, and is available at
-# http://www.eclipse.org/legal/epl-v20.html
-# SPDX-License-Identifier: EPL-2.0
-
-# @file    runner.py
-# @author  Lena Kalleske
-# @author  Daniel Krajzewicz
-# @author  Michael Behrisch
-# @author  Jakob Erdmann
-# @date    2009-03-26
-
 from __future__ import absolute_import
 from __future__ import print_function
+
+from collections import defaultdict
 
 import os
 import sys
 import optparse
 import random
+
+# car setting
+CarMaxSpeed = "15.0"
+
+# class decleration
+class Car:
+
+    MaxLengthToControl = 180
+
+    route_type_to_lane = {}
+    route_type_to_lane["route_WE"] = 1
+    route_type_to_lane["route_WN"] = 2
+    route_type_to_lane["route_WS"] = 0
+    route_type_to_lane["route_EW"] = 1
+    route_type_to_lane["route_EN"] = 0
+    route_type_to_lane["route_ES"] = 2
+    route_type_to_lane["route_NE"] = 2
+    route_type_to_lane["route_NW"] = 0
+    route_type_to_lane["route_NS"] = 1
+    route_type_to_lane["route_SE"] = 0
+    route_type_to_lane["route_SN"] = 1
+    route_type_to_lane["route_SW"] = 2
+
+    def __init__(self, route_type, VehId):
+        self.route_type = route_type
+        self.VehId = VehId
+        self.count = self.MaxLengthToControl * 10 / 15 
+
+    def new_to_road(self):
+        traci.vehicle.add(self.VehId, self.route_type, typeID="VehicleA", departSpeed=CarMaxSpeed, departLane=self.route_type_to_lane[self.route_type])
+        traci.vehicle.setSpeedMode(self.VehId, 0)
+
+    def step(self):
+        self.count = self.count - 1
+        traci.vehicle.setSpeed(self.VehId, float(CarMaxSpeed))
+        traci.vehicle.setSpeedMode(self.VehId, 0)
+        if self.count <= 0:
+            return True
+        else:
+            return False
+
+class RoadController:
+    def __init__(self):
+        self.waiting_dispatch = defaultdict(list)
+        self.on_road_car = []
+
+    def dispatch_car_from_waiting(self, step):
+        cars = self.waiting_dispatch[step]
+        for car in cars:
+            car.new_to_road()
+            self.on_road_car.append(car)
+
+    def assigned_car(self, step_to_roll_out, car):
+        self.waiting_dispatch[step_to_roll_out].append(car)
+
+    def step(self):
+        for car in self.on_road_car:
+            if car.step():
+                self.on_road_car.remove(car)
 
 # we need to import python modules from the $SUMO_HOME/tools directory
 if 'SUMO_HOME' in os.environ:
@@ -32,7 +77,7 @@ else:
 from sumolib import checkBinary  # noqa
 import traci  # noqa
 
-
+# simulation setting
 SimulationStepLength = 0.05
 SimulationPeriod = 1800
 SimulationEnding = 2000
@@ -41,29 +86,13 @@ print("Duration of Simulation(steps): " + str(SimulationDuration))
 
 def generate_routefile():
     random.seed(42)  # make tests reproducible
-    N = SimulationPeriod  # number of time steps
-    # demand per second from different directions (probabilities)
-    pWE = 1. / 10   # vehicles from west lane
-    pWN = 1. / 12
-    pWS = 1. / 30
-    pEW = 1. / 12   # vehicles from east lane
-    pEN = 1. / 16
-    pES = 1. / 25
-    pNE = 1. / 14   # vehicles from north lane
-    pNW = 1. / 16
-    pNS = 1. / 18
-    pSE = 1. / 12   # vehicles from south lane
-    pSN = 1. / 16
-    pSW = 1. / 20
     with open("data/cross.rou.xml", "w") as routes:
         print("""<routes>""", file = routes)
         # vehicle configuration of simulation
         print("""
-        <vType id="VehicleA" accel="3.5" decel="5.0" sigma="0" length="5" minGap="1.0" maxSpeed="15.0" speedFactor="1.0"
+        <vType id="VehicleA" accel="3.5" decel="5.0" sigma="0" length="5" minGap="1.0" maxSpeed="{}" speedFactor="1.0"
         guiShape="passenger" speedDev="0" />
-        <vType id="VehicleB" accel="3.5" decel="5.0" sigma="0" length="5" minGap="1.0" maxSpeed="15.0" speedFactor="1.0"
-        guiShape="passenger" speedDev="0" />
-        """, file=routes)
+        """.format(CarMaxSpeed), file=routes)
         #speedFactor="1.0"   speedDev="0"
         # route configuration of simulation
         print("""
@@ -80,103 +109,20 @@ def generate_routefile():
         <route id="route_SN" edges="L5 L6 L15 L16" />
         <route id="route_SW" edges="L5 L6 L3 L4" />
         """, file=routes)
-
-        # generate vehicles randomly
-        # vehNr = 0
-        # for i in range(N):
-        #     # vehicles dirving from west
-        #     if random.uniform(0, 1) < pWE:
-        #         print('    <vehicle id="vWE_%i" type="VehicleA" route="route_WE" depart="%i" />' % (
-        #             vehNr, i), file=routes)
-        #         vehNr += 1
-        #     if random.uniform(0, 1) < pWN:
-        #         print('    <vehicle id="vWN_%i" type="VehicleA" route="route_WN" depart="%i" />' % (
-        #             vehNr, i), file=routes)
-        #         vehNr += 1
-        #     if random.uniform(0, 1) < pWS:
-        #         print('    <vehicle id="vWS_%i" type="VehicleA" route="route_WS" depart="%i" />' % (
-        #             vehNr, i), file=routes)
-        #         vehNr += 1
-
-        #     # vehicles dirving from east
-        #     if random.uniform(0, 1) < pEW:
-        #         print('    <vehicle id="vEW_%i" type="VehicleA" route="route_EW" depart="%i" />' % (
-        #             vehNr, i), file=routes)
-        #         vehNr += 1
-        #     if random.uniform(0, 1) < pEN:
-        #         print('    <vehicle id="vEN_%i" type="VehicleA" route="route_EN" depart="%i" />' % (
-        #             vehNr, i), file=routes)
-        #         vehNr += 1
-        #     if random.uniform(0, 1) < pES:
-        #         print('    <vehicle id="vES_%i" type="VehicleA" route="route_ES" depart="%i" />' % (
-        #             vehNr, i), file=routes)
-        #         vehNr += 1
-
-        #     # vehicles dirving from south
-        #     if random.uniform(0, 1) < pSE:
-        #         print('    <vehicle id="vSE_%i" type="VehicleA" route="route_SE" depart="%i" />' % (
-        #             vehNr, i), file=routes)
-        #         vehNr += 1
-        #     if random.uniform(0, 1) < pSN:
-        #         print('    <vehicle id="vSN_%i" type="VehicleA" route="route_SN" depart="%i" />' % (
-        #             vehNr, i), file=routes)
-        #         vehNr += 1
-        #     if random.uniform(0, 1) < pSW:
-        #         print('    <vehicle id="vSW_%i" type="VehicleA" route="route_SW" depart="%i" />' % (
-        #             vehNr, i), file=routes)
-        #         vehNr += 1
-
-        #     # vehicles dirving from north
-        #     if random.uniform(0, 1) < pNE:
-        #         print('    <vehicle id="vNE_%i" type="VehicleA" route="route_NE" depart="%i" />' % (
-        #             vehNr, i), file=routes)
-        #         vehNr += 1
-        #     if random.uniform(0, 1) < pNS:
-        #         print('    <vehicle id="vNS_%i" type="VehicleA" route="route_NS" depart="%i" />' % (
-        #             vehNr, i), file=routes)
-        #         vehNr += 1
-        #     if random.uniform(0, 1) < pNW:
-        #         print('    <vehicle id="vNW_%i" type="VehicleA" route="route_NW" depart="%i" />' % (
-        #             vehNr, i), file=routes)
-        #         vehNr += 1
         print("</routes>", file=routes)
-        
-# The program looks like this
-#    <tlLogic id="0" type="static" programID="0" offset="0">
-# the locations of the tls are      NESW
-#        <phase duration="31" state="GrGr"/>
-#        <phase duration="6"  state="yryr"/>
-#        <phase duration="31" state="rGrG"/>
-#        <phase duration="6"  state="ryry"/>
-#    </tlLogic>
-
 
 def run():
     """execute the TraCI control loop"""
     random.seed(42)
     step = 0
-    # we start with phase 2 where EW has green
-    # traci.trafficlight.setPhase("0", 2)
-    traci.vehicle.add("newVeh", "route_WN", typeID="VehicleA", departSpeed="15.0", departLane="2")
-    traci.vehicle.setSpeedMode("newVeh", 0)
-    # traci.vehicle.add("newVeh2", "route_WE", typeID="VehicleA", departSpeed="10.0", departLane="1")
-    # traci.vehicle.setSpeedMode("newVeh2", 0)
-    # traci.vehicle.add("newVeh3", "route_WS", typeID="VehicleA", departSpeed="10.0", departLane="0")
-    # traci.vehicle.setSpeedMode("newVeh3", 0)
-    # traci.vehicle.add("newVeh4", "route_WE", typeID="VehicleB", departSpeed="15.0", departLane="1", depart="2")
-    # traci.vehicle.setSpeedMode("newVeh4", 0)
+    road_control = RoadController()
+    road_control.assigned_car(30, Car("route_WS", "0"))
+    road_control.assigned_car(40, Car("route_WN", "1"))
+    road_control.assigned_car(50, Car("route_WE", "2"))
     while step < 1000:
+        road_control.dispatch_car_from_waiting(step)
+        road_control.step()
         traci.simulationStep()
-        # traci.vehicle.setSpeed("newVeh", 10.0)
-        # traci.vehicle.setSpeedMode("newVeh", 0)
-        # traci.vehicle.setSpeed("newVeh2", 10.0)
-        # # traci.vehicle.setSpeedMode("newVeh2", 0)
-        # traci.vehicle.setSpeed("newVeh3", 10.0)
-        # # traci.vehicle.setSpeedMode("newVeh3", 0)
-        # traci.vehicle.setSpeed("newVeh4", 15.0)
-        # traci.vehicle.setSpeedMode("newVeh4", 0)
-        # if step % 10 == 0:
-        #     traci.vehicle.setSpeed("0", random.uniform(30, 50))
         step += 1
     traci.close()
     sys.stdout.flush()
@@ -209,3 +155,81 @@ if __name__ == "__main__":
     traci.start([sumoBinary, "-c", "data/cross.sumocfg",
                              "--tripinfo-output", "tripinfo.xml"])
     run()
+
+# N = SimulationPeriod  # number of time steps
+# # demand per second from different directions (probabilities)
+# pWE = 1. / 10   # vehicles from west lane
+# pWN = 1. / 12
+# pWS = 1. / 30
+# pEW = 1. / 12   # vehicles from east lane
+# pEN = 1. / 16
+# pES = 1. / 25
+# pNE = 1. / 14   # vehicles from north lane
+# pNW = 1. / 16
+# pNS = 1. / 18
+# pSE = 1. / 12   # vehicles from south lane
+# pSN = 1. / 16
+# pSW = 1. / 20
+# generate vehicles randomly
+# vehNr = 0
+# for i in range(N):
+#     # vehicles dirving from west
+#     if random.uniform(0, 1) < pWE:
+#         print('    <vehicle id="vWE_%i" type="VehicleA" route="route_WE" depart="%i" />' % (
+#             vehNr, i), file=routes)
+#         vehNr += 1
+#     if random.uniform(0, 1) < pWN:
+#         print('    <vehicle id="vWN_%i" type="VehicleA" route="route_WN" depart="%i" />' % (
+#             vehNr, i), file=routes)
+#         vehNr += 1
+#     if random.uniform(0, 1) < pWS:
+#         print('    <vehicle id="vWS_%i" type="VehicleA" route="route_WS" depart="%i" />' % (
+#             vehNr, i), file=routes)
+#         vehNr += 1
+
+#     # vehicles dirving from east
+#     if random.uniform(0, 1) < pEW:
+#         print('    <vehicle id="vEW_%i" type="VehicleA" route="route_EW" depart="%i" />' % (
+#             vehNr, i), file=routes)
+#         vehNr += 1
+#     if random.uniform(0, 1) < pEN:
+#         print('    <vehicle id="vEN_%i" type="VehicleA" route="route_EN" depart="%i" />' % (
+#             vehNr, i), file=routes)
+#         vehNr += 1
+#     if random.uniform(0, 1) < pES:
+#         print('    <vehicle id="vES_%i" type="VehicleA" route="route_ES" depart="%i" />' % (
+#             vehNr, i), file=routes)
+#         vehNr += 1
+
+#     # vehicles dirving from south
+#     if random.uniform(0, 1) < pSE:
+#         print('    <vehicle id="vSE_%i" type="VehicleA" route="route_SE" depart="%i" />' % (
+#             vehNr, i), file=routes)
+#         vehNr += 1
+#     if random.uniform(0, 1) < pSN:
+#         print('    <vehicle id="vSN_%i" type="VehicleA" route="route_SN" depart="%i" />' % (
+#             vehNr, i), file=routes)
+#         vehNr += 1
+#     if random.uniform(0, 1) < pSW:
+#         print('    <vehicle id="vSW_%i" type="VehicleA" route="route_SW" depart="%i" />' % (
+#             vehNr, i), file=routes)
+#         vehNr += 1
+
+#     # vehicles dirving from north
+#     if random.uniform(0, 1) < pNE:
+#         print('    <vehicle id="vNE_%i" type="VehicleA" route="route_NE" depart="%i" />' % (
+#             vehNr, i), file=routes)
+#         vehNr += 1
+#     if random.uniform(0, 1) < pNS:
+#         print('    <vehicle id="vNS_%i" type="VehicleA" route="route_NS" depart="%i" />' % (
+#             vehNr, i), file=routes)
+#         vehNr += 1
+#     if random.uniform(0, 1) < pNW:
+#         print('    <vehicle id="vNW_%i" type="VehicleA" route="route_NW" depart="%i" />' % (
+#             vehNr, i), file=routes)
+#         vehNr += 1
+
+# traci.vehicle.add("newVeh", "route_WN", typeID="VehicleA", departSpeed="15.0", departLane="2")
+# traci.vehicle.setSpeedMode("newVeh", 0)
+# traci.vehicle.setSpeed("newVeh", 10.0)
+# traci.vehicle.setSpeedMode("newVeh", 0)
